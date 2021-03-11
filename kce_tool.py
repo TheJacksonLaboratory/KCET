d1e1b3c7506ec01bc9a8ed2fceaa9fa1ad2384aa
Peter Robinson <peter.robinson@jax.org>
2021-03-10 21:30:15 -0500

    cleanup

--- kce_tool.py
+++ kce_tool.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 
 
-from kcet import CTParserByPhase, UnTargetedKinases, TestTrainingPredictionGenerator
+from kcet import CTParserByPhase
 
 
 class KinaseCancerEmbeddingTool(object):
@@ -17,10 +17,7 @@ class KinaseCancerEmbeddingTool(object):
 
 The kcet commands are:
    byphase      clinical trials by phase
-   targeted     get list of targeted and untargeted kinases
    kinaselist   get list of all kinases
-   merge        merge PKI/trial and PKI/kinase information
-   ttp          write positive, negative, and prediction datasets
 ''')
         parser.add_argument('command', help='Subcommand to run')
         # parse_args defaults to [1:] for args, but you need to
@@ -83,38 +80,9 @@ The kcet commands are:
         fh.close()
         print("[INFO] Wrote %d kinases to %s" % (len(pki_list), args.outfilename))
 
-    def targeted(self):
-        parser = argparse.ArgumentParser(description='extract list of untargeted protein kinases and write to file')
-        parser.add_argument('-y', '--year', default= None, type = int, help='The first year a drug was in testing.')
-        parser.add_argument('-c','--clinical_trials', required=True, help="path to the clinical_trials_by_phase file")
-        args = parser.parse_args(sys.argv[2:])
-        targeted = UnTargetedKinases(clinical_trials_by_phase=args.clinical_trials, year=args.year)
-        year = targeted.get_target_year()
-        targeted_kinases_filename = "targeted_kinases_%d.tsv" % year
-        untargeted_kinases_filename = "untargeted_kinases_%d.tsv" % year
-        targeted_kinases_phase_4_filename = "targeted_kinases_phase_4_%d.tsv" % year
-        targeted_kinases = targeted.get_targeted_kinases_with_gene_id()
-        targeted_kinases.to_csv(targeted_kinases_filename,sep='\t')
-        untargeted_kinases = targeted.get_untargeted_kinases_with_gene_id()
-        untargeted_kinases.to_csv(untargeted_kinases_filename, sep='\t')
-        targeted_kinases_phase_4 = targeted.get_targeted_kinases_with_gene_id_phase_4()
-        targeted_kinases_phase_4.to_csv(targeted_kinases_phase_4_filename, sep='\t')
 
 
-    def ttp(self):
-        parser = argparse.ArgumentParser(description='Generate training, test, and prediction datasets')
-        parser.add_argument('-y', '--year', default= None, type = int, help='The first year a drug was in testing.')
-        parser.add_argument('-c','--clinical_trials', required=True, help="path to the clinical_trials_by_phase file")
-        parser.add_argument('-f','--factor', default=10, help="factor by which we have more negative than positive examples")
-        parser.add_argument('--prefix',type=str, default='KCET',help='The prefix for the outfiles.')
-        args = parser.parse_args(sys.argv[2:])
-        parser = TestTrainingPredictionGenerator(clinical_trials=args.clinical_trials, year=args.year)
-        filename = "%s_positive_%d.tsv" % (args.prefix, parser.get_year())
-        parser.write_positive_dataset(outfilename=filename)
-        filename = "%s_negative_%d.tsv" % (args.prefix, parser.get_year())
-        parser.write_negative_dataset(outfilename=filename)
-        filename = "%s_prediction_%d.tsv" % (args.prefix, parser.get_year())
-        parser.write_prediction_dataset(outfilename=filename)
+
 
 if __name__ == '__main__':
     KinaseCancerEmbeddingTool()
