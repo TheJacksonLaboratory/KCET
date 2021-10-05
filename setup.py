da07d487137205e6dbc583ddf28a30436654600e
Peter Robinson <peter.robinson@jax.org>
2021-10-05 09:52:10 -0400

    version 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='kcet',
-   version='0.2.0',
+   version='0.3.0',
    author='Vida Ravanmehr, Peter N Robinson',
    author_email='vida.ravanmehr@jax.org,peter.robinson@jax.org',
    packages=['kcet'],
